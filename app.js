39a75cb61af5662f7fcd6cc2c77ece52b7339ae1
shahzodbekFrontend <shahzodbekrahmonjonov83@gmail.com>
2022-12-13 11:49:53 +0500

    YTYT

---
+++ app.js
@@ -0,0 +1,25 @@
+const btn1 = document.querySelector('.btn1')
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const hiddin = document.querySelector('hiddin')
+const cloes = document.querySelector('#cloes')
+const menu_right = document.querySelector('.menu-right')
+const menu_close = document.querySelector('#menu-close')
+const menu = document.querySelector('.menu')
+
+btn1.addEventListener("click", () => {
+    overlay.classList.remove("hiddin")
+    modal.setAttribute('style', 'transform: translate(-50%, -50%)')
+})
+cloes.addEventListener("click", () => {
+    overlay.classList.add("hiddin")
+    modal.setAttribute('style', 'transform: translate(-50%, -500%)')
+})
+menu.addEventListener("click", () => {
+    overlay.classList.remove("hiddin")
+    menu_right.setAttribute('style', 'transform: translateX(0)')
+})
+menu_close.addEventListener("click", () => {
+    overlay.classList.add("hiddin")
+    menu_right.setAttribute('style', 'transform: translateX(500px)')
+})
